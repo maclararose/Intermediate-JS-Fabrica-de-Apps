ea6a46cf0d1a90b12c6f1631290e1e4084847d56
maclararose <maria.uespi@outlook.com>
2020-03-22 22:14:11 -0300

    Mouse Events

---
+++ project 1 - mouse event/script.js
@@ -0,0 +1,28 @@
+function mouseDown(){
+    console.log("O mouse foi apertado");
+}
+
+function mouseUp(){
+    console.log("Soltei");
+}
+
+function mouseOver(){
+    console.log("Passou o mouse");
+}
+
+function mouseOut(){
+    console.log("Tirou o mouse");
+}
+function mouseMove(){
+    console.log("Movendo o mouse");
+}
+
+function onClick(){
+    console.log("Clicou no botao esquerdo");
+}
+
+function contextMenu(){
+    console.log("Clicou no botao direito");
+
+    return false;
+}
