d094f321c517e9ad04cf68489a62d8d4f19a7b65
maclararose <maria.uespi@outlook.com>
2020-04-01 14:58:39 -0300

    Keyboard events

---
+++ project 2 - keyboard event/script.js
@@ -0,0 +1,11 @@
+function keyDown(event){
+    console.log("Pressed... "+event.keyCode);
+}
+
+function keyUp(event){
+    console.log("Let go...");
+}
+
+function keyPress(event){
+    console.log("You pressed a keyboard...");
+}
