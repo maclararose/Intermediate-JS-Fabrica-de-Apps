da7514b31ccb42c450fe9830168e25be2b1c381f
maclararose <maria.uespi@outlook.com>
2020-04-02 17:47:32 -0300

    Form events

---
+++ project 4 - form events/script.js
@@ -0,0 +1,11 @@
+function focus(){
+    console.log("Focused...");
+}
+
+function blur(){
+    console.log("Blur...");
+}
+
+function change(object){
+    console.log("City Selected..." + object.value);
+}
